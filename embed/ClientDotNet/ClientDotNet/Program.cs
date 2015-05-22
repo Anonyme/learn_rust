c65f090f82d70ce61119b2549c056569d3f353d7
Anonyme <anonyme.code@gmail.com>
2015-05-22 16:00:49 +0200

    Add .Net client

---
+++ embed/ClientDotNet/ClientDotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ClientDotNet
+{
+    class Program
+    {
+        [DllImport("embed.dll")]
+        internal static extern void process();
+
+        static void Main(string[] args)
+        {
+            process();
+        }
+    }
+}
