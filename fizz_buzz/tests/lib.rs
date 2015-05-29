440b6351bf83785f3b4ae777f89b4cdf26f3bf85
Anonyme <anonyme.code@gmail.com>
2015-05-29 08:57:48 +0200

    Add tests to fizz_buzz

--- fizz_buzz/tests/lib.rs
+++ fizz_buzz/tests/lib.rs
@@ -4,3 +4,18 @@ extern crate fizz_buzz;
 fn one_neither_fizz_neither_buzz() {
     assert_eq!("1", fizz_buzz::fizz_buzz(1));
 }
+
+#[test]
+fn three_is_fizz() {
+    assert_eq!("Fizz", fizz_buzz::fizz_buzz(3));
+}
+
+#[test]
+fn five_is_buzz() {
+    assert_eq!("Buzz", fizz_buzz::fizz_buzz(5));
+}
+
+#[test]
+fn fiveteen_is_fizzbuzz() {
+    assert_eq!("FizzBuzz", fizz_buzz::fizz_buzz(15));
+}
