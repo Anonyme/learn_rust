34b4337f9b49458ac4965a05b9887763a86e0f73
Anonyme <anonyme.code@gmail.com>
2015-06-02 13:23:32 +0200

    Replace ifs by pattern matching and guards

--- fizz_buzz/src/lib.rs
+++ fizz_buzz/src/lib.rs
@@ -10,13 +10,10 @@
 /// assert_eq!("FizzBuzz", fizz_buzz(15));
 /// ```
 pub fn fizz_buzz(num: i32) -> String {
-    if num % 3 == 0 && num % 5 == 0 {
-        "FizzBuzz".to_string()
-    } else if num % 3 == 0 {
-        "Fizz".to_string()
-    } else if num % 5 == 0 {
-        "Buzz".to_string()
-    } else {
-        num.to_string()
+    match num {
+        n if n % 3 == 0 && n % 5 == 0 => "FizzBuzz".to_string(),
+        n if n % 3 == 0 => "Fizz".to_string(),
+        n if n % 5 == 0 => "Buzz".to_string(),
+        _ => num.to_string()
     }
 }
